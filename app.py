edebaaddf878763ae37bc7cfd8aa54fde8de86bf
AayushSingh0011 <aayush0707patel@gmail.com>
2026-01-28 00:38:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -84,5 +84,9 @@ def dashboard():
     return render_template("dashboard.html", result=result)
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
